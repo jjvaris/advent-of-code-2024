128882e47f3cbd9bc4888e66ffa1d5f408b34a9f
Jouni Varis <jouni.varis@gofore.com>
2024-12-11 22:07:02 +0200

    day 11 refactoring

--- 11/solution.js
+++ 11/solution.js
@@ -12,32 +12,35 @@ function part2() {
 }
 
 function getStoneCount(stones, blinks) {
-  return stones.reduce((sum, stone) => (sum += cachedBlink(stone, blinks)), 0);
+  return stones.reduce(
+    (sum, stone) => (sum += memoizedBlink(stone, blinks)),
+    0
+  );
 }
 
-const cachedBlink = memoize(blink);
+const memoizedBlink = memoize(blink);
 
 function blink(stone, depth) {
   if (depth === 0) {
     return 1;
   }
   if (stone === 0) {
-    return cachedBlink(1, depth - 1);
+    return memoizedBlink(1, depth - 1);
   }
   const stoneString = `${stone}`;
   if (stoneString.length % 2 === 0) {
     return (
-      cachedBlink(
+      memoizedBlink(
         parseInt(stoneString.slice(0, stoneString.length / 2)),
         depth - 1
       ) +
-      cachedBlink(
+      memoizedBlink(
         parseInt(stoneString.slice(stoneString.length / 2, stoneString.length)),
         depth - 1
       )
     );
   }
-  return cachedBlink(stone * 2024, depth - 1);
+  return memoizedBlink(stone * 2024, depth - 1);
 }
 
 part1();
